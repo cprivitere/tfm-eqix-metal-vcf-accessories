0b1db59397fb8f41a9d9724b6385515cb5f5f944
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-08-16 20:31:01 +0000

    more updates
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -9,18 +9,22 @@ resource "equinix_metal_device" "bastion" {
     gateway_address = var.vm_mgmt_gateway,
   })
 
-  operating_system    = "ubuntu_22_04"
+  operating_system    = "ubuntu_24_04"
   plan                = var.metal_bastion_plan
   metro               = var.metal_metro
   project_ssh_key_ids = [module.ssh.equinix_metal_ssh_key_id]
+
 }
 
 resource "equinix_metal_port" "bastion_bond0" {
-  port_id  = [for p in equinix_metal_device.bastion.ports : p.id if p.name == "bond0"][0]
-  layer2   = false
-  bonded   = true
-  vlan_ids = [data.equinix_metal_vlan.vm-mgmt.vlan_id]
+  depends_on      = [equinix_metal_device.bastion]
+  port_id         = [for p in equinix_metal_device.bastion.ports : p.id if p.name == "bond0"][0]
+  layer2          = false
+  bonded          = true
+  vlan_ids        = [data.equinix_metal_vlan.vm-mgmt.vlan_id]
+  reset_on_delete = true
 }
+
 
 resource "equinix_metal_project" "vcf" {
   count           = var.create_project ? 1 : 0
@@ -36,4 +40,9 @@ data "equinix_metal_vlan" "vm-mgmt" {
 module "ssh" {
   source     = "./modules/ssh/"
   project_id = var.metal_project_id
+}
+
+resource "equinix_metal_bgp_session" "bastion_bgp" {
+  device_id      = equinix_metal_device.bastion.id
+  address_family = "ipv4"
 }
