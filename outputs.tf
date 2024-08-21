c98db61797ade329d411e1f8aa576014c3dd2d98
Chris Privitere <23177737+cprivitere@users.noreply.github.com>
2024-08-21 17:41:13 +0000

    update outputs to have cloudbuilder host
    Signed-off-by: Chris Privitere <23177737+cprivitere@users.noreply.github.com>

--- outputs.tf
+++ outputs.tf
@@ -9,5 +9,5 @@ output "bastion_public_ip" {
 }
 output "ssh_forward_command" {
   description = "SSH port forward command to use to connect to the Prism GUI"
-  value       = "ssh -L 8000:172.16.11.101:443 -i ${module.ssh.ssh_private_key} root@${equinix_metal_device.bastion.access_public_ipv4}"
+  value       = "ssh -L 8000:172.16.11.101:443 -L 8100:172.16.10.3:443 -i ${module.ssh.ssh_private_key} root@${equinix_metal_device.bastion.access_public_ipv4}"
 }
